8008847dcd41e932a97edb69c99c1c1a59e0931b
Adrian Bieniek <kontakt@abieniek.dev>
2024-03-11 19:33:14 +0100

    Initialization and configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,10 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        primary: "#ef233c",
+        secondary: "#2b2d42",
+        content: "#1e1e1e",
       },
     },
   },
