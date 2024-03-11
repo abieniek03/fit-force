61ac640d79d9a2c80955b1d1ab3a952330714d11
Adrian Bieniek <kontakt@abieniek.dev>
2024-03-11 20:17:46 +0100

    Change clerk language and theme

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -1,7 +1,10 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function DashboardPage() {
   return (
     <main>
       <h1>Hello from DASHBOARD!👋</h1>
+      <UserButton afterSignOutUrl="/" />
     </main>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { ILayout } from "./_types/types";
 
 import { ClerkProvider } from "@clerk/nextjs";
+import { plPL } from "@clerk/localizations";
 
 const font = Poppins({ subsets: ["latin"], weight: ["400", "700"] });
 
@@ -13,8 +14,19 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({ children }: Readonly<ILayout>) {
+  const theme = {
+    primary: "#ef233c",
+    secondary: "#2b2d42",
+    content: "#1e1e1e",
+  };
+
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      localization={plPL}
+      appearance={{
+        variables: { colorPrimary: theme.primary, colorText: theme.secondary },
+      }}
+    >
       <html lang="pl">
         <body className={`${font.className} text-content`}>{children}</body>
       </html>
