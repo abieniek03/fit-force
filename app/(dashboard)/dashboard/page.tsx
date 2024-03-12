b7b3bdd56a49dd578a8824db0aa6069664f8e910
Adrian Bieniek <kontakt@abieniek.dev>
2024-03-12 19:16:08 +0100

    Add current date in dashboard

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -1,5 +1,7 @@
 import { Metadata } from "next/types";
 import { currentUser } from "@clerk/nextjs";
+import { format } from "date-fns";
+import { pl } from "date-fns/locale";
 
 import DashboardPageTitle from "@/app/_components/dashboard/DashboardPageTitile";
 
@@ -10,9 +12,21 @@ export const metadata: Metadata = {
 export default async function DashboardPage() {
   const user = await currentUser();
 
+  const getCurrentDate = () => {
+    const formattedDate = format(new Date(), "EEEE, d MMMM yyyy", {
+      locale: pl,
+    });
+
+    return `${formattedDate.slice(0, 1).toLocaleUpperCase()}${formattedDate.slice(1)}`;
+  };
   return (
     <>
-      <DashboardPageTitle>Cześć {user?.firstName}!👋</DashboardPageTitle>
+      <header className="border-b pb-4">
+        <DashboardPageTitle>Cześć {user?.firstName}!👋</DashboardPageTitle>
+        <div>
+          <span>{getCurrentDate()}</span>
+        </div>
+      </header>
     </>
   );
 }
