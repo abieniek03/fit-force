96e1b0e5066ccaffa20f1732f13b421462b6c98c
Adrian Bieniek <kontakt@abieniek.dev>
2024-03-20 19:26:50 +0100

    Add SignOutButton

--- app/_components/dashboard/DashboardMenu.tsx
+++ app/_components/dashboard/DashboardMenu.tsx
@@ -8,6 +8,8 @@ import clsx from "clsx";
 
 import logo from "@/app/_assets/images/logo-secondary.svg";
 import { IoIosClose } from "react-icons/io";
+import { BiLogOut } from "react-icons/bi";
+import { SignOutButton } from "@clerk/nextjs";
 
 interface IMenuItems {
   id: string;
@@ -37,40 +39,51 @@ export default function DashboardMenu() {
       <nav
         className={clsx(
           isOpen ? "left-0" : "-left-full",
-          "absolute top-0 h-screen w-full bg-primary transition-all duration-500 lg:sticky lg:left-0 lg:top-0 lg:block lg:w-[260px] lg:rounded-e-xl lg:transition-none",
+          "absolute top-0 flex h-screen w-full flex-col justify-between bg-primary transition-all duration-500 lg:sticky lg:left-0 lg:top-0 lg:flex lg:w-[260px] lg:rounded-e-xl lg:transition-none",
         )}
       >
-        <div className="flex items-center justify-between">
-          <Image
-            src={logo}
-            alt="FitForce"
-            width={100}
-            height={30}
-            className="my-5 ml-5 lg:w-1/2"
-          />
-          <button
-            onClick={() => setIsOpen(false)}
-            className={"p-2 text-3xl text-accent lg:hidden"}
-          >
-            <IoIosClose />
-          </button>
-        </div>
         <div>
-          {menuItems.map((el, index) => (
-            <Link
+          <div className="flex items-center justify-between">
+            <Image
+              src={logo}
+              alt="FitForce"
+              width={100}
+              height={30}
+              className="my-5 ml-5 lg:w-1/2"
+            />
+            <button
               onClick={() => setIsOpen(false)}
-              href={el.path}
-              key={index}
-              className={clsx(
-                "block p-2 pl-6",
-                el.id === pathname.split("/")[1]
-                  ? "bg-accent font-bold"
-                  : "text-accent",
-              )}
+              className={"p-2 text-3xl text-accent lg:hidden"}
             >
-              {el.label}
-            </Link>
-          ))}
+              <IoIosClose />
+            </button>
+          </div>
+
+          <div>
+            {menuItems.map((el, index) => (
+              <Link
+                onClick={() => setIsOpen(false)}
+                href={el.path}
+                key={index}
+                className={clsx(
+                  "block p-2 pl-6",
+                  el.id === pathname.split("/")[1]
+                    ? "bg-accent font-bold"
+                    : "text-accent",
+                )}
+              >
+                {el.label}
+              </Link>
+            ))}
+          </div>
+        </div>
+        <div className="mx-auto mb-5 w-full py-2 pl-6 text-accent">
+          <SignOutButton>
+            <span className="flex cursor-pointer items-center gap-2">
+              Wyloguj się
+              <BiLogOut className=" text-xl" />
+            </span>
+          </SignOutButton>
         </div>
       </nav>
     </>
