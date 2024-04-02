264a92f252ff6e8985f9a70f5292547b2e662778
Adrian Bieniek <kontakt@abieniek.dev>
2024-04-02 14:07:04 +0200

    Add loading animation and loading page

---
+++ app/loading.tsx
@@ -0,0 +1,9 @@
+import { Loading } from "./_components/Loading";
+
+export default function LoadingPage() {
+  return (
+    <main className="flex h-screen items-center justify-center">
+      <Loading />
+    </main>
+  );
+}
