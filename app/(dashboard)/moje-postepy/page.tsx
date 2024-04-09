584f9a3ddef5f6f16fd0bb5b0b179c9c3e07f451
Adrian Bieniek <kontakt@abieniek.dev>
2024-04-09 14:29:37 +0200

    Handle loading my progress active training camp

--- app/(dashboard)/moje-postepy/page.tsx
+++ app/(dashboard)/moje-postepy/page.tsx
@@ -1,19 +1,45 @@
-import { redirect } from "next/navigation";
-import { fetchData, ITrainingCamp } from "@/app/_utils/fetch/fetchData";
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { useQuery } from "@tanstack/react-query";
 
 import { MyProgressNotExist } from "@/app/_components/dashboard/my-progress/MyProgressNotExist";
 import { Loading } from "@/app/_components/Loading";
+import { getSessionToken } from "@/app/_utils/helpers/getSessionToken";
+import axios from "@/app/_utils/axios/axiosInstance";
 
-export default async function MyProgressRootPage() {
-  const { data }: { data: ITrainingCamp } = await fetchData(
-    "/training-camp?latest=true",
-  );
+export default function MyProgressRootPage() {
+  const router = useRouter();
+  const sessionToken = getSessionToken();
 
-  if (data.id === undefined) return <MyProgressNotExist />;
+  const getCampData = async () => {
+    try {
+      const response = await axios.get("/training-camp?latest=true", {
+        headers: {
+          Authorization: `Bearer ${sessionToken}`,
+        },
+      });
 
-  if (data.id) {
-    redirect(`moje-postepy/${data.id}`);
-  }
+      return response.data;
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const { data, isPending, isError, isSuccess } = useQuery({
+    queryKey: ["camp-data"],
+    queryFn: getCampData,
+  });
+
+  useEffect(() => {
+    if (isSuccess && data.data.id !== undefined) {
+      const id = data.data.id;
+      router.push(`moje-postepy/${id}`);
+    }
+  }, [isSuccess, data]);
+
+  if (!isPending && (isError || !data.data.id)) return <MyProgressNotExist />;
 
   return <Loading />;
 }
