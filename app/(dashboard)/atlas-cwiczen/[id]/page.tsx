e0f7ce3c2d471249b6a48ecf6785a9671ca811c2
SzymekNawrocki <129281704+SzymekNawrocki@users.noreply.github.com>
2024-04-16 21:04:58 +0200

    Added exercises cms

--- app/(dashboard)/atlas-cwiczen/[id]/page.tsx
+++ app/(dashboard)/atlas-cwiczen/[id]/page.tsx
@@ -1,122 +1,49 @@
 import Image from "next/image";
 import { DashboardPageTitle } from "@/app/_components/dashboard/DashboardPageTitle";
-import { IServerComponentProps } from "@/app/_types/types";
+import { gql } from "graphql-request";
+import { gqlClient } from "@/app/_utils/gql/gqlClient";
 
-const exercises = [
-  {
-    title: "Przysiad ze sztangą",
-    group: "czworoglowy",
-    imageUrl:
-      "https://cdn.pixabay.com/photo/2015/07/02/10/23/training-828741_1280.jpg",
-    description:
-      "Stań prosto, trzymając sztangę na karku za pomocą odpowiedniego uchwytu Opuszczaj ciało w dół, jakbyś siadał na krzesło, zachowując plecy proste Wróć do pozycji wyjściowej, napinając mięśnie nóg.",
-  },
-  {
-    title: "Wykroki ze sztangą",
-    group: "czworoglowy",
-    imageUrl:
-      "https://cdn.pixabay.com/photo/2015/07/02/10/23/training-828741_1280.jpg",
-    description:
-      "Stań prosto, trzymając sztangę na plecach za pomocą odpowiedniego uchwytu. Zrób duży krok do przodu, opuszczając kolano drugiej nogi w dół. Wróć do pozycji wyjściowej i powtórz na drugą stronę.",
-  },
-  {
-    title: "Wyciskanie nogami na maszynie do wyciskania",
-    group: "czworoglowy",
-    imageUrl:
-      "https://cdn.pixabay.com/photo/2015/07/02/10/23/training-828741_1280.jpg",
-    description:
-      "Usiądź na maszynie do wyciskania z nogami na podnośniku. Wypchnij podnośnik w górę, prostując nogi. Powoli opuszczaj podnośnik do pozycji wyjściowej.",
-  },
-  {
-    title: "Wspięcia na palce",
-    group: "czworoglowy",
-    imageUrl:
-      "https://cdn.pixabay.com/photo/2015/07/02/10/23/training-828741_1280.jpg",
-    description:
-      "To ćwiczenie skupia się głównie na mięśniach łydek, ale również angażuje czworoglowe Stań prosto na podłożu płaskim, trzymając się np. poręczy. Podnoś obie pięty, unosząc się na palcach.Opuszczaj się powoli z powrotem na płaskość stóp.",
-  },
-  {
-    title: "Przysiady ze sztangielkami",
-    group: "czworoglowy",
-    imageUrl:
-      "https://cdn.pixabay.com/photo/2015/07/02/10/23/training-828741_1280.jpg",
-    description:
-      "Trzymając sztangielki wzdłuż ciała, stań prosto. Opuszczaj ciało w dół, jak przy tradycyjnym przysiadzie, zachowując plecy proste. Wróć do pozycji wyjściowej.",
-  },
-  {
-    title: "Proste wznoszenie nóg",
-    group: "czworoglowy",
-    imageUrl:
-      "https://cdn.pixabay.com/photo/2015/07/02/10/23/training-828741_1280.jpg",
-    description:
-      "Leż na ławce z głową w dół, trzymając się krawędzi ławki. Unosź nogi prosto do góry, napinając mięśnie czworogłowe. Opuszczaj nogi kontrolowanie do pozycji wyjściowej.",
-  },
-  {
-    title: "Przykładowe Ćwiczenie 1",
-    group: "czworoglowy",
-    imageUrl:
-      "https://cdn.pixabay.com/photo/2015/07/02/10/23/training-828741_1280.jpg",
-    description: "Opis ćwiczenia 1",
-  },
-  {
-    title: "Przykładowe Ćwiczenie 1",
-    group: "czworoglowy",
-    imageUrl:
-      "https://cdn.pixabay.com/photo/2015/07/02/10/23/training-828741_1280.jpg",
-    description: "Opis ćwiczenia 1",
-  },
-  {
-    title: "Przykładowe Ćwiczenie 1",
-    group: "czworoglowy",
-    imageUrl:
-      "https://cdn.pixabay.com/photo/2015/07/02/10/23/training-828741_1280.jpg",
-    description: "Opis ćwiczenia 1",
-  },
-  {
-    title: "Przykładowe Ćwiczenie 1",
-    group: "czworoglowy",
-    imageUrl:
-      "https://cdn.pixabay.com/photo/2015/07/02/10/23/training-828741_1280.jpg",
-    description: "Opis ćwiczenia 1",
-  },
-  {
-    title: "Przykładowe Ćwiczenie 2",
-    group: "dwugłowe uda, pośladki",
-    imageUrl:
-      "https://cdn.pixabay.com/photo/2015/07/02/10/23/training-828741_1280.jpg",
-    description: "Opis ćwiczenia 2",
-  },
-];
+interface IExercise {
+  group: string;
+  title: string;
+  description: string;
+  image: {
+    url: string;
+  };
+}
 
-export default function ExercisePage({ params }: IServerComponentProps) {
-  const { id } = params;
-  const muscleName = id;
+export default async function ExerciseSection() {
+  const query = gql`
+    {
+      allExercises {
+        group
+        title
+        description
+        image {
+          url
+        }
+      }
+    }
+  `;
 
-  const filteredExercises = exercises.filter(
-    (exercise) => exercise.group.toLowerCase() === muscleName.toLowerCase(),
-  );
-
-  if (filteredExercises.length === 0)
-    return (
-      <p className="text-center text-secondary">
-        Brak dostępnych ćwiczeń dla tej partii mięśniowej.
-      </p>
-    );
+  const data: any = await gqlClient.request(query);
+  const { allExercises } = data;
+ 
 
   return (
     <div className="max-w-screen-3xl  mx-auto mt-2  rounded-md bg-accent p-4 text-center">
       <div className="mb-24 text-center">
-        <DashboardPageTitle>{muscleName}</DashboardPageTitle>
+        <DashboardPageTitle>{allExercises[0].group}</DashboardPageTitle>
       </div>
       <div className="grid gap-16">
-        {filteredExercises.map((exercise, index) => (
+        {allExercises.map((exercise: IExercise, index: number) => (
           <div
             key={index}
             className="flex flex-col items-center rounded-md bg-accent p-4 shadow-md lg:w-full lg:flex-row"
           >
             <div className="mb-4 lg:mb-0 lg:mr-4">
               <Image
-                src={exercise.imageUrl}
+                src={exercise.image?.url}
                 alt={exercise.title}
                 width={400}
                 height={200}
