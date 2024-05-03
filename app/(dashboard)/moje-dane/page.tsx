5d93e03f1b358348fc9f76bd63367d738c32c036
Adrian Bieniek <kontakt@abieniek.dev>
2024-05-03 17:06:28 +0200

    Add content to my data page

--- app/(dashboard)/moje-dane/page.tsx
+++ app/(dashboard)/moje-dane/page.tsx
@@ -1,8 +1,10 @@
 import { Metadata } from "next/types";
 import Image from "next/image";
 import { currentUser } from "@clerk/nextjs";
+import { differenceInYears } from "date-fns";
 
 import { DashboardPageTitle } from "@/app/_components/dashboard/DashboardPageTitle";
+import { fetchData } from "@/app/_utils/fetch/fetchData";
 
 export const metadata: Metadata = {
   title: "Moje dane",
@@ -10,6 +12,20 @@ export const metadata: Metadata = {
 
 export default async function DashboardPage() {
   const user = await currentUser();
+
+  const userProfile = await fetchData("/user-profile");
+
+  const latestWeight = await fetchData("/weight/bmi");
+
+  const calcAge = (birthDate: Date) => {
+    return differenceInYears(new Date(), birthDate);
+  };
+
+  const calcBmi = () => {
+    const bmi =
+      latestWeight.data.weight / Math.pow(userProfile.data.height / 100, 2);
+    return bmi.toFixed(1);
+  };
 
   return (
     <>
@@ -25,8 +41,26 @@ export default async function DashboardPage() {
             className="mb-2 rounded-full"
           />
           <div>
-            <p className="text-xl text-content/50">{user?.firstName}, 20 lat</p>
+            <p className="text-xl text-content/50">
+              {user?.firstName}, {calcAge(userProfile.data.birthDate)}
+            </p>
           </div>
+        </div>
+        <div className="mx-auto max-w-xs opacity-75">
+          <table>
+            <tr>
+              <td className="w-full">Wzrost</td>
+              <td className="text-right">{userProfile.data.height}</td>
+            </tr>
+            <tr>
+              <td className="w-full">Waga</td>
+              <td className="text-right"> {latestWeight.data.weight}</td>
+            </tr>
+            <tr>
+              <td className="w-full">BMI</td>
+              <td className="text-right"> {calcBmi()}</td>
+            </tr>
+          </table>
         </div>
       </div>
     </>
